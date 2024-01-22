57b5b4268b50a6543481614de42d52e776121b11
Tanay Godse <tanaygodse@gmail.com>
2024-01-21 22:20:24 -0800

    AST, Parser, let statements

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,8 @@
 package lexer
 
 import (
+	"fmt"
+
 	"github.com/tanaygodse/interpreter/token"
 
 	"testing"
@@ -147,6 +149,7 @@ func TestNextToken(t *testing.T) {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
+		fmt.Println(tok)
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
